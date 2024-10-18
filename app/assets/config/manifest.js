b38aeb4ecb7443e273698c767b83d3cc73881e0f
ryoko <f.ryoko1014@gmail.com>
2024-10-19 08:33:00 +0900

    画面レイアウト変更

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link application.css
 //= link bootstrap.min.css
+//= link custom_styles.css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
