202a6c97a51f52d14975b342811160fc72fdadb3
ryoko <f.ryoko1014@gmail.com>
2024-10-30 02:34:48 +0900

    モデルに関係性を追加

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,6 @@
 class Food < ApplicationRecord
     belongs_to :partner
+    has_many :remainders, as: :activity, dependent: :destroy 
+    
+    accepts_nested_attributes_for :remainders
 end
--- app/models/remainder.rb
+++ app/models/remainder.rb
@@ -1,3 +1,4 @@
 class Remainder < ApplicationRecord
     belongs_to :partner
+    belongs_to :activity, polymorphic: true  # 複数の活動（Food, Walk, Medicationなど）を扱う
 end
