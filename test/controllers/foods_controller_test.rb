735b8eaa68e1fb3731c4c03e4fe5dd47dc8a2d3e
ryoko <f.ryoko1014@gmail.com>
2024-10-30 02:45:52 +0900

    Javascriptの設定変更によるプリコンパイル

---
+++ test/controllers/foods_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class FoodsControllerTest < ActionDispatch::IntegrationTest
+  test "should get edit" do
+    get foods_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get foods_update_url
+    assert_response :success
+  end
+end
