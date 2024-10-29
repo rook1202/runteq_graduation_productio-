d972b1764712d9a8ea246028973e0d926c1f7cb6
ryoko <f.ryoko1014@gmail.com>
2024-10-30 02:40:00 +0900

    コントローラーに設定追加

---
+++ app/controllers/medications_controller.rb
@@ -0,0 +1,49 @@
+class MedicationsController < ApplicationController
+  before_action :set_partner, only: [:edit, :update, :add_remainder_field]
+  before_action :set_medication, only: [:edit, :update]
+
+  def edit
+    @medication.remainders.build if @medication.remainders.blank?
+  end
+
+  def add_remainder_field
+    @remainder = @partner.medications.build.remainders.build  # 新しいRemainderインスタンスを作成
+    render partial: "time_medication", locals: { remainder: @remainder, partner_id: @partner.id }  # remainderオブジェクトを渡す
+  end
+
+  def update
+    # 空のtimeを持つremainderを除外する
+    filtered_remainders = params[:medication][:remainders_attributes].reject do |_, r|
+      r[:time].blank?
+    end
+    # フィルタリングしたremaindersを再構築
+    params[:medication][:remainders_attributes] = filtered_remainders
+
+    if @medication.update(medication_params)
+      flash[:success] = 'おくすりの情報が更新されました。'
+      redirect_to partner_path(@partner)
+    else
+      flash.now[:danger] = '更新が失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_partner
+    @partner = current_user.partners.find(params[:partner_id])
+  end
+
+  def set_medication
+    @medication = @partner.medications.find_by(id: params[:id])
+    raise ActiveRecord::RecordNotFound, "Medication not found" if @medication.nil?
+  end
+
+  def medication_params
+    params.require(:medication).permit(
+      :name, :amount, :place, :clinic, :note,
+      remainders_attributes: [:id, :time, :notification_status, :partner_id, :_destroy]
+    )
+  end
+  
+end
---
+++ app/controllers/walks_controller.rb
@@ -0,0 +1,49 @@
+class WalksController < ApplicationController
+  before_action :set_partner, only: [:edit, :update, :add_remainder_field]
+  before_action :set_walk, only: [:edit, :update]
+
+  def edit
+    @walk.remainders.build if @walk.remainders.blank?
+  end
+
+  def add_remainder_field
+    @remainder = @partner.walks.build.remainders.build  # 新しいRemainderインスタンスを作成
+    render partial: "time_walk", locals: { remainder: @remainder, partner_id: @partner.id }  # remainderオブジェクトを渡す
+  end
+
+  def update
+    # 空のtimeを持つremainderを除外する
+    filtered_remainders = params[:walk][:remainders_attributes].reject do |_, r|
+      r[:time].blank?
+    end
+    # フィルタリングしたremaindersを再構築
+    params[:walk][:remainders_attributes] = filtered_remainders
+
+    if @walk.update(walk_params)
+      flash[:success] = 'さんぽの情報が更新されました。'
+      redirect_to partner_path(@partner)
+    else
+      flash.now[:danger] = '更新が失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_partner
+    @partner = current_user.partners.find(params[:partner_id])
+  end
+
+  def set_walk
+    @walk = @partner.walks.find_by(id: params[:id])
+    raise ActiveRecord::RecordNotFound, "walk not found" if @walk.nil?
+  end
+
+  def walk_params
+    params.require(:walk).permit(
+      :time, :note,
+      remainders_attributes: [:id, :time, :notification_status, :partner_id, :_destroy]
+    )
+  end
+
+end
