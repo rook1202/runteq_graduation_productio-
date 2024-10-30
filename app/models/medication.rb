2ba6eed6f81044562b37d28f77282ebd08c53704
ryoko <f.ryoko1014@gmail.com>
2024-10-30 23:55:46 +0900

    timeカラムデータを削除する設定をモデルと編集用フォームに追加

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -2,5 +2,5 @@ class Medication < ApplicationRecord
     belongs_to :partner
     has_many :remainders, as: :activity, dependent: :destroy 
     
-    accepts_nested_attributes_for :remainders
+    accepts_nested_attributes_for :remainders, allow_destroy: true
 end
