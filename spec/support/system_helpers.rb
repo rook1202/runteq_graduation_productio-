f31ff799bf9a369e09bd3b1e10470bc861989f9d
ryoko <f.ryoko1014@gmail.com>
2024-11-22 20:26:05 +0900

    ログインモジュールの設定

--- spec/support/system_helpers.rb
+++ spec/support/system_helpers.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 module SystemHelpers
-    def login_as(user)
-      visit root_path
-      fill_in 'email', with: user.email
-      fill_in 'password', with: '12345678'
-      click_button 'ログイン'
-    end
+  def login_as(user)
+    visit login_path
+    fill_in 'email', with: user.email
+    fill_in 'password', with: '12345678'
+    click_button 'ログイン'
+    expect(page).to have_current_path(root_path) # ログイン確認
+
+  end
 end
