71753d5242acc2c63f19c0f88a72dcca9295cb8a
ryoko <f.ryoko1014@gmail.com>
2024-11-23 01:11:05 +0900

    ユーザー名変更のActionを追加

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,27 @@
+class SettingsController < ApplicationController
+  def show
+  end
+
+  def name_change
+  end
+
+  def email_change
+  end
+
+  def update
+    if current_user.update(user_params)
+      flash[:success] = '変更が完了しました'
+      redirect_to settings_path
+    else
+      flash.now[:danger] = '変更が失敗しました'
+      render :name_change, status: :unprocessable_entity
+    end
+  end
+
+end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
