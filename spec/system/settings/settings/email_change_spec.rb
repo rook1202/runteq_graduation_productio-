b9429c06c9c77820c098f97280316e49af94180c
ryoko <f.ryoko1014@gmail.com>
2024-11-25 15:38:36 +0900

    Rubocopの自動修正

--- spec/system/settings/settings/email_change_spec.rb
+++ spec/system/settings/settings/email_change_spec.rb
@@ -5,9 +5,9 @@ require 'rails_helper'
 RSpec.describe 'メールアドレス変更ページ', type: :system do
   let(:user) { create(:user) }
 
-    it 'ページが正しく表示される' do
-      login_as(user)
-      visit email_change_settings_path
-      expect(page).to have_current_path(email_change_settings_path)
-    end
+  it 'ページが正しく表示される' do
+    login_as(user)
+    visit email_change_settings_path
+    expect(page).to have_current_path(email_change_settings_path)
+  end
 end
