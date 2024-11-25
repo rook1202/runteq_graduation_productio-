ddedac8a877d6946201d901b7f22b9ac0d2ade66
ryoko <f.ryoko1014@gmail.com>
2024-11-25 15:32:14 +0900

    メール送信設定を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,4 +72,9 @@ Rails.application.configure do
 
   # ホスト許可リストをクリアし、すべてのホストからのリクエストを許可する
   config.hosts.clear
+
+  # letter_opener設定
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
