f268150cdb09bb8ae3569b759f8c6913bb2e5353
ryoko <f.ryoko1014@gmail.com>
2024-11-26 14:22:35 +0900

    メールの送信設定を記述

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,6 @@
 class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = user
-    @reset_link = edit_password_reset_url(token: @user.reset_password_token)
     mail(to: @user.email, subject: 'パスワードリセットのご案内')
   end
 end
