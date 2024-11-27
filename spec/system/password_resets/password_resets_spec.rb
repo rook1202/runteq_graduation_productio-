bf06941fe8fc3163047d594acf230896d9088011
ryoko <f.ryoko1014@gmail.com>
2024-11-27 22:31:48 +0900

    パスワード変更のnewのルーティングを修正

--- spec/system/password_resets/password_resets_spec.rb
+++ spec/system/password_resets/password_resets_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe 'パスワード再設定メール送信ページ', type: :system
   it 'ログイン画面から入る（未ログイン）' do
     visit login_path
     click_on 'パスワードを忘れましたか？'
-    expect(page).to have_current_path(new_password_reset)
+    expect(page).to have_current_path(new_password_reset_path)
   end
 
   it 'パスワードが変更できる' do
-    visit new_password_reset
+    visit new_password_reset_path
     fill_in 'email', with: user.email
     click_button 'メール送信'
     expect(page).to have_content('パスワード再設定のリンクをメールで送信しました。'), 'フラッシュメッセージ「パスワード再設定のリンクをメールで送信しました。」が表示されていません'
