a3859b3f439ea955de2fe24ca2e5208fb30d8a31
ryoko <f.ryoko1014@gmail.com>
2024-11-29 02:01:50 +0900

    CapybaraCookieHelperの作成と読み込み設定、SystemHelpersは変更なし

--- spec/support/capybara_cookie_helper.rb
+++ spec/support/capybara_cookie_helper.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-module SystemHelpers
-  def login_as(user)
-    visit login_path
-    fill_in 'email', with: user.email
-    fill_in 'password', with: '12345678'
-    click_button 'ログイン'
-    expect(page).to have_current_path(root_path) # ログイン確認
-  end
-end
-
 module CapybaraCookieHelper
   def get_rack_cookie(cookie_name)
     browser = page.driver.browser
