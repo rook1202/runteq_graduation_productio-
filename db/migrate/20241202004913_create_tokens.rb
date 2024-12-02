bd0db8fb277e911b7e3e71cfa304c2235988c58b
ryoko <f.ryoko1014@gmail.com>
2024-12-02 21:10:02 +0900

    tokenテーブルとPartnerShareテーブルの作成

---
+++ db/migrate/20241202004913_create_tokens.rb
@@ -0,0 +1,14 @@
+# Partner共有のためのトークン発行・管理するテーブルを作成する
+class CreateTokens < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tokens do |t|
+      t.string :token, null: false, unique: true
+      t.references :user, null: false, foreign_key: true
+      t.references :partner, foreign_key: true
+      t.datetime :expiration_date, null: false
+      t.timestamps
+    end
+
+    add_index :tokens, :token, unique: true
+  end
+end
