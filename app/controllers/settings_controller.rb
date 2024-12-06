0a0c79258c45247bcfb2e55e61f04ba3e71f0861
ryoko <f.ryoko1014@gmail.com>
2024-12-06 16:23:48 +0900

    自動修正

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,12 +6,12 @@ class SettingsController < ApplicationController
     # 自分が共有しているユーザー
     users_shared_by_me = User.joins(:partner_shares)
                              .where(partner_shares: { shared_by: current_user.id })
-  
+
     # 自分に共有しているユーザー
     users_shared_with_me = User.where(
       id: PartnerShare.where(user_id: current_user.id).select(:shared_by)
     )
-  
+
     # 結合して重複を排除
     @shared_users = (users_shared_by_me + users_shared_with_me).uniq
   end
