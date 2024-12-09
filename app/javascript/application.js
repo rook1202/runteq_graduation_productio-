e869cee0c1a7289feac52d90a92d81ea6c120172
ryoko <f.ryoko1014@gmail.com>
2024-12-09 23:38:29 +0900

    petのデフォルト画像を変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,6 +7,7 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
 import "share_partner"
+import "service_worker_registration"
 
 
 function addRemainderFields(activityType) {
