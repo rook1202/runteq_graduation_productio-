40762f493b155fa8ac8b0c0981e8338b698767ac
ryoko <f.ryoko1014@gmail.com>
2024-12-12 11:45:53 +0900

    sidekiqの有効化

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
