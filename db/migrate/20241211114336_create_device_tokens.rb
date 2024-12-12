02b45d48181a24fad2e04896e22d0ddec9dca00d
ryoko <f.ryoko1014@gmail.com>
2024-12-12 11:32:19 +0900

    デバイストークン保存テーブル作成

---
+++ db/migrate/20241211114336_create_device_tokens.rb
@@ -0,0 +1,10 @@
+class CreateDeviceTokens < ActiveRecord::Migration[7.0]
+  def change
+    create_table :device_tokens do |t|
+      t.string :token, null: false
+      t.references :partner, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_02_004942) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_11_114336) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_02_004942) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "device_tokens", charset: "utf8mb3", force: :cascade do |t|
+    t.string "token", null: false
+    t.bigint "partner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["partner_id"], name: "index_device_tokens_on_partner_id"
   end
 
   create_table "foods", charset: "utf8mb3", force: :cascade do |t|
@@ -155,6 +163,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_02_004942) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "device_tokens", "partners"
   add_foreign_key "foods", "partners"
   add_foreign_key "medications", "partners"
   add_foreign_key "partner_shares", "partners"
