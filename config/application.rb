8eca7d1f75508055231833f44849a7b9b540617a
ryoko <f.ryoko1014@gmail.com>
2024-12-13 01:01:08 +0900

    タイムゾーンをTokyoに変更

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     config.active_job.queue_adapter = :sidekiq
   end
