f5db4bae438e9e44e22a9923d025bea954f63c2a
ryoko <f.ryoko1014@gmail.com>
2024-12-13 00:47:51 +0900

    スケジューリング登録・削除メソッド追加

--- app/models/remainder.rb
+++ app/models/remainder.rb
@@ -4,4 +4,24 @@
 class Remainder < ApplicationRecord
   belongs_to :partner
   belongs_to :activity, polymorphic: true # 複数の活動（Food, Walk, Medicationなど）を扱う
+
+  def schedule_notification
+    notification_time = DateTime.parse(time).utc # UTCに変換
+    job = SendNotificationJob.set(wait_until: notification_time).perform_later(id)
+    update_column(:job_id, job.job_id)
+  end
+
+  def cancel_notification
+    return unless job_id # ジョブIDが存在しない場合は処理しない
+  
+    scheduled_jobs = Sidekiq::ScheduledSet.new
+    scheduled_jobs.each do |entry|
+      if entry.item['args'].first['job_id'] == job_id
+        entry.delete
+        puts "Deleted job: #{job_id}"
+      end
+    end
+  
+    update_column(:job_id, nil) # キャンセル後にジョブIDをクリア
+  end
 end
