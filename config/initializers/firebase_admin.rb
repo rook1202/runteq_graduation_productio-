42e6e4ef1f95452d19546dad3d4118dc98b59a9f
ryoko <f.ryoko1014@gmail.com>
2024-12-14 16:12:45 +0900

    firebase_admin.rbのサーバー側の初期化を修正

--- config/initializers/firebase_admin.rb
+++ config/initializers/firebase_admin.rb
@@ -1,9 +1,18 @@
 require "firebase-admin-sdk"
 
-# Firebase Admin SDKの認証情報を設定
+# Firebase Admin SDK の認証情報を設定
 credentials_path = Rails.root.join('config', 'petnote-14a7e-firebase-adminsdk-wcojg-c82e3dced1.json')
 
-# JSONファイルが存在しない場合のエラー処理
+# JSON ファイルが存在しない場合のエラー処理
 raise "Firebase credentials file not found at #{credentials_path}" unless File.exist?(credentials_path)
 
-CREDENTIALS = JSON.parse(File.read(credentials_path)) # 定数として保持
+# サービスアカウントを使って Firebase アプリを初期化
+creds = Firebase::Admin::Credentials.from_file(credentials_path)
+FIREBASE_APP = Firebase::Admin::App.new(credentials: creds)
+
+# Messaging クライアントを直接利用可能にするためのヘルパーメソッドを提供
+module FirebaseAdmin
+  def self.messaging
+    FIREBASE_APP.messaging
+  end
+end
