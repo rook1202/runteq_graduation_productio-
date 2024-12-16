c567a76fc1f0c8bded6118b44674ea8e2a96d69e
ryoko <f.ryoko1014@gmail.com>
2024-12-16 18:08:58 +0900

    FirebaseからOneSignalへ移行するためservice_worker_registration.jsの内容を変更、不要になったファイルを削除

--- app/javascript/service_worker_registration.js
+++ app/javascript/service_worker_registration.js
@@ -1,12 +1,9 @@
-console.log("Service Worker の登録 が実行されました");
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/firebase-messaging-sw.js')
-        .then(registration => {
-          console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-          console.error('Service Worker registration failed:', error);
-        });
+  navigator.serviceWorker.register('/OneSignalSDKWorker.js', { scope: '/' })
+    .then(function(registration) {
+      console.log('Service Worker registered:', registration);
+    })
+    .catch(function(error) {
+      console.error('Service Worker registration failed:', error);
     });
-};
+}
