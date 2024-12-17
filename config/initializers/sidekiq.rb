749328e31615ffd3e4717de253931ffb4c2004b1
ryoko <f.ryoko1014@gmail.com>
2024-12-17 23:18:41 +0900

    sidekiq.rbの設定変更

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,7 @@
-if Rails.env.production?
-    Sidekiq.configure_server do |config|
-      config.redis = { url: 'redis://localhost:6379/0' }
-    end
-  
-    Sidekiq.configure_client do |config|
-      config.redis = { url: 'redis://localhost:6379/0' }
-    end
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://redis:6379/0' } # コンテナ名を'redis'とする場合
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://redis:6379/0' }
 end
