4fad38fbaf61690b2b684cf55fe0ddeb8ca1da8a
ryoko <f.ryoko1014@gmail.com>
2024-12-17 23:31:10 +0900

    webpack.config.jsの内容を変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/service_worker_registration.js', // エントリーポイント
+  entry: './src/', // エントリーポイント
   output: {
-    path: path.resolve(__dirname, 'public'), // 出力先ディレクトリ
-    filename: 'service_worker.js', // 出力ファイル名
+    path: path.resolve(__dirname, 'public/pack'), // 出力先ディレクトリ
+    filename: '', // 出力ファイル名
   },
   mode: 'production', // モード
 };
