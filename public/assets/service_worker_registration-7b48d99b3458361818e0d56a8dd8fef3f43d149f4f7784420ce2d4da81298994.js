5db1058d83d451acda8ec3e87f576ed8c35bb6b1
ryoko <f.ryoko1014@gmail.com>
2024-12-17 23:37:46 +0900

    再プリコンパイル

---
+++ public/assets/service_worker_registration-7b48d99b3458361818e0d56a8dd8fef3f43d149f4f7784420ce2d4da81298994.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/OneSignalSDKWorker.js', { scope: '/' })
+    .then(function(registration) {
+      console.log('Service Worker registered:', registration);
+    })
+    .catch(function(error) {
+      console.error('Service Worker registration failed:', error);
+    });
+};
