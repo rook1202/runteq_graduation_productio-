b8d83055e3c5b9d1cab35a1fe96d58ff557e4609
ryoko <f.ryoko1014@gmail.com>
2024-12-20 00:17:19 +0900

    お問い合わせフォームページのテストコードを作成

--- spec/factories/device_tokens.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :device_token do
-    token { "MyString" }
-    partner { nil }
-  end
-end
---
+++ spec/mailers/contact_form_mailer_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ContactFormMailer, type: :mailer do
+  
+  describe 'お問い合わせ' do
+    let(:user) { create(:user) }
+    let(:title) { 'お問い合わせ件名' }
+    let(:body) { 'お問い合わせ内容です！' }
+
+    it 'お問い合わせが送信できるか' do
+      expect do
+        ContactFormMailer.contact_form(user, title, body).deliver_now
+      end.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+
+    it '正しい内容がメールに含まれているか' do
+      email = ContactFormMailer.contact_form(user, title, body).deliver_now
+
+      expect(email.to).to eq(['petnote0808@gmail.com']) # 管理者のメールアドレス
+      expect(email.subject).to eq('お問い合わせ件名')
+      expect(email.body.to_s).to include('お問い合わせ内容です！')
+      expect(email.body.to_s).to include('てすと') # ユーザー名を確認
+      expect(email.body.to_s).to include('test@example.com') # ユーザーのメールアドレスを確認
+    end
+  end
+
+end
