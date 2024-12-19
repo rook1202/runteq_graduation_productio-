be38a5612fdec131edc60cc8a74bc393d6587a53
ryoko <f.ryoko1014@gmail.com>
2024-12-19 13:50:35 +0900

    password変更時のメール送信とチェックをメソッドを追加

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -9,8 +9,10 @@ class PasswordResetsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
+    Rails.logger.info("Submitted email: #{params[:email]}")
     if @user
       @user.deliver_reset_password_instructions!
+      UserMailer.reset_password_email(@user).deliver_now
       logout
       flash[:success] = 'パスワード再設定のリンクをメールで送信しました。'
       redirect_to login_path
@@ -33,13 +35,19 @@ class PasswordResetsController < ApplicationController
     @user = load_user_from_token
     return redirect_invalid_token if @user.blank?
 
-    if change_user_password
-      flash[:success] = 'パスワードが変更されました。'
-      redirect_to login_path
-    else
-      flash.now[:danger] = 'パスワードの変更ができませんでした。'
-      render :edit
+    # バリデーションチェックの内容を出力
+    password = params[:user][:password]
+    password_confirmation = params[:user][:password_confirmation]
+    if validate_passwords(password, password_confirmation)
+      if change_user_password
+        flash[:success] = 'パスワードが変更されました。'
+        redirect_to login_path
+        return
+      else
+        flash.now[:danger] = 'パスワードの変更ができませんでした。'
+      end
     end
+    render :edit, status: :unprocessable_entity
   end
 
   private
@@ -63,5 +71,34 @@ class PasswordResetsController < ApplicationController
   def logout
     # セッションをクリアしてログアウト処理を実行
     reset_session
+  end
+
+  def validate_passwords(password, password_confirmation)
+  if password.blank? || password_confirmation.blank?
+    flash.now[:danger] = 'パスワードとパスワード確認を入力してください。'
+    return false
+  elsif password.length < 6
+    flash.now[:danger] = 'パスワードは6文字以上で入力してください。'
+    return false
+  elsif password != password_confirmation
+    flash.now[:danger] = 'パスワードと確認用パスワードが一致しません。'
+    return false
+  end
+  true
+  end
+
+  def check_validations(user)
+    Rails.logger.debug '=== バリデーションチェック ==='
+    user.class.validators.each do |validator|
+      Rails.logger.debug "チェック内容: #{validator.attributes} - #{validator.options}"
+      validator.attributes.each do |attribute|
+        if user.errors[attribute].empty?
+          Rails.logger.debug "  - #{attribute}: OK"
+        else
+          Rails.logger.debug "  - #{attribute}: NG (#{user.errors[attribute].join(', ')})"
+        end
+     end
+    end
+    Rails.logger.debug '==========================='
   end
 end
