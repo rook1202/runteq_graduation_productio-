bc12bc07d8e00b2aae79ec152d616e81d95c2072
ryoko <f.ryoko1014@gmail.com>
2024-12-19 15:11:29 +0900

    ヘッダーの戻るリンク変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,44 @@
 
 # ApplicationHelperモジュールは、アプリケーション全体で使用されるヘルパーメソッドを提供します。
 module ApplicationHelper
+
+    def back_link
+        case
+        when controller_name == 'partners' && action_name == 'edit'
+          link_to(partner_path(@partner), class: 'navbar-brand') do
+            content_tag(:i, '', class: 'fas fa-chevron-left')
+          end
+        when %w(foods walks medications).include?(controller_name) && action_name == 'edit'
+          link_to(partner_path(@partner), class: 'navbar-brand') do
+            content_tag(:i, '', class: 'fas fa-chevron-left')
+          end
+        when controller_name == 'partners' && action_name == 'show'
+          link_to(root_path, class: 'navbar-brand') do
+            content_tag(:i, '', class: 'fas fa-chevron-left')
+          end
+        when controller_name == 'partners' && action_name == 'new'
+          link_to(root_path, class: 'navbar-brand') do
+            content_tag(:i, '', class: 'fas fa-chevron-left')
+          end
+        when controller_name == 'users' && action_name == 'new'
+          link_to(login_path, class: 'navbar-brand') do
+            content_tag(:i, '', class: 'fas fa-chevron-left')
+          end
+        when controller_name == 'settings' && %w(name_change privacy_policy terms_of_use news).include?(action_name)
+          link_to(settings_path, class: 'navbar-brand') do
+            content_tag(:i, '', class: 'fas fa-chevron-left')
+          end
+        when controller_name == 'email_changes' && action_name == 'new'
+          link_to(settings_path, class: 'navbar-brand') do
+            content_tag(:i, '', class: 'fas fa-chevron-left')
+          end
+        when controller_name == 'contacts' && action_name == 'new'
+          link_to(settings_path, class: 'navbar-brand') do
+            content_tag(:i, '', class: 'fas fa-chevron-left')
+          end
+        else
+          nil
+        end
+    end
+
 end
