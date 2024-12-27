650b8b10c1c27e83205648d87b95c85eaa3b53dd
ryoko <f.ryoko1014@gmail.com>
2024-12-28 01:53:08 +0900

    チュートリアルを表示させないためfactories/users.rbにshowed_tutorialの項目を追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,11 +7,17 @@ FactoryBot.define do
     email { 'test@example.com' }
     password { '12345678' }
     password_confirmation { '12345678' }
+    showed_tutorial { true }
 
     # 2人目のユーザー用の設定
     trait :other_user do
       name { 'てすと2' }
       email { 'test2@example.com' }
+    end
+
+    # チュートリアルを表示したい場合
+    trait :with_tutorial do
+      showed_tutorial { false }
     end
   end
 end
