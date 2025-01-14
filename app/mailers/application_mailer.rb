4530ba6ea7b7e645e4b08fb60b80856dd5532f84
ryoko <f.ryoko1014@gmail.com>
2025-01-14 13:42:52 +0900

    mailerを使用可能に設定

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,16 +2,8 @@
 
 # メール設定
 class ApplicationMailer < ActionMailer::Base
-  before_action :force_recipient_in_production
 
   default from: 'petnote0808@gmail.com'
   layout 'mailer'
 
-  private
-
-  def force_recipient_in_production
-    return unless Rails.env.production?
-
-    mail(to: 'petnote0808@gmail.com')
-  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,7 +123,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'www.mypetnote-family.com', protocol: 'https' }
 
   # メール送信を有効化
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
 
   # メール送信エラーを無視しない
   config.action_mailer.raise_delivery_errors = true
